fa867cbc8df0fd65e3b114956df1932f64d669b6
loonuh <cjluna1@asu.edu>
2014-05-24 20:31:53 -0700

    Think I fixed the bugs...

--- Chapter1_IsingModel.py
+++ Chapter1_IsingModel.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-iterMax = 1000
+iterMax = 100
 iterNum = 0
 
-gsize = 16
-#numZeros = 3
+gsize = 32
+numNOnes = 1
 
 k = 1
 startT = .5;endT = 5;stepT = .5;
@@ -15,6 +15,12 @@ T = .5
 arrT = np.linspace(startT,endT,numT)
 
 statMat = np.random.randint(2,size=[gsize,gsize])*2-1
+
+arrOnes = np.ones(gsize**2 - numNOnes)
+arrNOnes = np.ones(numNOnes)*-1
+
+statMat = np.reshape(np.hstack((arrOnes,arrNOnes)),[gsize,gsize])
+
 print(np.size(statMat))
 statMatflip = np.copy(statMat) 
 plt.figure()
@@ -26,42 +32,34 @@ while iterNum < iterMax:
     print('\n')
     print('Iteration:'+str(iterNum))
     
-    xcorflip = np.random.randint(gsize)
-    ycorflip = np.random.randint(gsize)
-    statMatflip[xcorflip,ycorflip] = -statMat[xcorflip,ycorflip]
-    
+    xcor = np.random.randint(gsize)
+    ycor = np.random.randint(gsize)
+    statMatflip[xcor,ycor] = -statMat[xcor,ycor]
     energy = 0 
     energyflip = 0
 
-    energyS = np.zeros([gsize,gsize])
-    energySflip = np.zeros([gsize,gsize])
-
-    for xcor in range(gsize):
-        for ycor in range(gsize):
-            if xcor == gsize-1:
-                xcor_p = 0
-            else:
-                xcor_p = xcor+1
-            if xcor == 0:
-                xcor_m = gsize-1
-            else:
-                xcor_m = xcor-1
-            if ycor == gsize-1:
-                ycor_p = 0
-            else:
-                ycor_p = ycor+1
-            if ycor == 0:
-                ycor_m = gsize-1
-            else:
-                ycor_m = ycor-1
+    if xcor == gsize-1:
+        xcor_p = 0
+    else:
+        xcor_p = xcor+1
+    if xcor == 0:
+        xcor_m = gsize-1
+    else:
+        xcor_m = xcor-1
+    if ycor == gsize-1:
+        ycor_p = 0
+    else:
+        ycor_p = ycor+1
+    if ycor == 0:
+        ycor_m = gsize-1
+    else:
+        ycor_m = ycor-1
             
-            coordNeighbors = [[xcor_m,ycor],[xcor_p,ycor],[xcor,ycor_m],[xcor,ycor_p]]
-            for coords in coordNeighbors: 
-                energy += statMat[coords[0],coords[1]]*statMat[xcor,ycor]
-                energyS[xcor,ycor] += statMat[coords[0],coords[1]]*statMat[xcor,ycor]
-                
-                energySflip[xcor,ycor] += statMatflip[coords[0],coords[1]]*statMatflip[xcor,ycor]
-                energyflip += statMatflip[coords[0],coords[1]]*statMatflip[xcor,ycor]
+    coordNeighbors = [[xcor_m,ycor],[xcor_p,ycor],[xcor,ycor_m],[xcor,ycor_p]]
+            
+    for coords in coordNeighbors: 
+        energy += statMat[coords[0],coords[1]]*statMat[xcor,ycor]
+        energyflip += statMatflip[coords[0],coords[1]]*statMatflip[xcor,ycor]
             
     xchi = (np.random.random(1))
     energy = -energy/2
@@ -69,25 +67,33 @@ while iterNum < iterMax:
     
     Magnetization = np.abs(float(np.sum(np.sum(statMat)))/(gsize**2))
     M.append(Magnetization)
+
     deltaE = energyflip-energy
     
     print('Energy,Energyflip: '+str(energy)+','+str(energyflip))
-    if deltaE < 0:
+    if float(deltaE) < 0:
         statMat = np.copy(statMatflip)
+        print('deltaE 1: ' +str(deltaE))
         print('flipped it')
+    #elif deltaE == 0:
+    #    print('no action')
+    #    continue
     else:
         W = np.exp(-deltaE/T/k)
-        if float(W) <= xchi:
+        if float(W) >= xchi:
             statMat=np.copy(statMatflip)
+            print('deltaE 2: ' +str(deltaE))
+            print('W,xchi: ' +str(W)+','+str(xchi))
             print('flipped it')
         else:
             statMat = np.copy(statMat)
             print('no action')
-    if np.mod(iterNum,100) == 0:
+    if np.mod(iterNum,1) == 0:
         plt.pcolor(np.transpose(statMat))
         plt.axis("tight")
         plt.draw()
 
 plt.figure()
 plt.plot(M)
+plt.draw()
 plt.show()
